bf71f6939a4c0107354a8b3221a315043bc3ea05
nurik0708 <aaikossya09@gmail.com>
2023-11-26 09:03:18 +0600

    Добавьте файлы проекта.

---
+++ Modul9/HDD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9
+{
+    class HDD : Storage
+    {
+        private double usbSpeed;
+        private int partitionCount;
+        private double partitionSize;
+        public HDD(string name, string model, double usbSpeed, int partitionCount, double partitionSize)
+        {
+            Name = name;
+            Model = model;
+            this.usbSpeed = usbSpeed;
+            this.partitionCount = partitionCount;
+            this.partitionSize = partitionSize;
+        }
+        public override double GetMemory()
+        {
+            return partitionCount * partitionSize;
+        }
+
+        public override void CopyData(double dataSize)
+        {
+            Console.WriteLine($"Transferring data to a removable HDD. Data Size: {dataSize} Gb");
+        }
+
+        public override void GetFreeSpaceInfo()
+        {
+            Console.WriteLine($"Free space on removable HDD: {GetMemory()} Gb");
+        }
+        public override void GetDeviceInfo()
+        {
+            Console.WriteLine($"Information about removable HDD: {Name}, {Model}, speed USB: {usbSpeed} Giga/sec, Number of sections: {partitionCount}");
+        }
+    }
+}
---
+++ Modul9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage[] devices = new Storage[]
+            {
+            new Flash("FlDr", "Model009", 5.0, 88),
+            new DVD("DVD-SH", "LiteOn", 5.0, true),
+            new HDD("ExHDD", "Seagate", 4.0, 4, 800)
+                };
+            double totalMemory = 0;
+            foreach (var device in devices)
+            {
+                totalMemory += device.GetMemory();
+            }
+            double dataSize = 565;
+            foreach (var device in devices)
+            {
+                device.CopyData(dataSize);
+            }
+            Console.WriteLine($"Total memory: {totalMemory} Gb");
+            double copySpeed = 5;
+            double copyTime = dataSize / copySpeed;
+            Console.WriteLine($"Time, to copy elements: {copyTime} sec");
+            double totalDataSize = dataSize;
+            foreach (var device in devices)
+            {
+                double deviceMemory = device.GetMemory();
+                double devicesNeeded = Math.Ceiling(totalDataSize / deviceMemory);
+                Console.WriteLine($"To transfer data, you will need {devicesNeeded} type device {device.GetType().Name}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
